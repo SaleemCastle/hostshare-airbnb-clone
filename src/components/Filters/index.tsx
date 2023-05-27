d557dc0d029f139703710079842eddc9d8ffbf77
Saleem Castle <saleemcastle94@gmail.com>
2023-05-27 13:30:04 -0500

    early implementation of header and filter comps

---
+++ src/components/Filters/index.tsx
@@ -0,0 +1,148 @@
+import React, { ReactElement } from 'react';
+import { IoBedOutline, IoPinOutline } from 'react-icons/io5';
+import { AiOutlineRight } from 'react-icons/ai';
+
+const Filters = (): ReactElement => {
+    return (
+        <div className='flex flex-row justify-between items-center pr-10'>
+            <div className='grid grid-flow-col gap-12 mt-10 px-10 justify-start max-w-[95%] overflow-hidden'>
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 text-xs font-semibold group-hover:text-black group-hover:transition-all'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 text-xs font-semibold group-hover:text-black group-hover:transition-all'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div><div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div><div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div><div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div><div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div><div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+                <div className='flex flex-col items-center justify-between h-20 cursor-pointer group py-3 border-b border-transparent hover:border-gray-300'>
+                    <IoBedOutline className='text-3xl text-gray-600 group-hover:text-black group-hover:transition-all'/>
+                    <h3 className='text-center text-gray-600 group-hover:text-black group-hover:transition-all text-xs font-semibold'>Rooms</h3>
+                </div>
+
+            </div>
+            
+            <div className='mt-10 flex flex-row items-center justify-center'>
+                <div className='w-8 h-8 border rounded-full flex items-center justify-center mr-3'>
+                    <AiOutlineRight className='text-xs'/>
+                </div>
+
+                <div className='py-4 px-4 rounded-xl flex flex-row ml-3 border items-center'>
+                    <span className='mr-1 font-semibold text-xs relative -top-px'>
+                        <IoPinOutline className='rotate-90 top-1 absolute'/>
+                        <IoPinOutline className='rotate-[270deg] ' />
+                    </span>
+                    <h3 className='ml-1 font-semibold text-xs'>Filters</h3>
+                </div>
+            </div>
+        </div> 
+     );
+}
+ 
+export default Filters;
